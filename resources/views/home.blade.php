25bcb26093e3140d0c16abc452cf5cdd1e43b675
Duco Lindner <duckelekuuk@gmail.com>
2017-09-30 03:28:54 +0200

    Added font-awesome and updated footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,16 @@
     <create-link></create-link>
 
     <div class="footer navbar-fixed-bottom">
-        <div class="text-center">Duco Lindner © 2017</div>
+        <div class="container">
+            <div class="text-center">
+                <h5>Duco Lindner © 2017</h5>
+            </div>
+            <div class="text-center">
+                <ul class="list-inline">
+                    <li><a href="https://twitter.com/duckelekuuk" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a></li>
+                    <li><a href="https://github.com/duckelekuuk" target="_blank"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 @endsection
