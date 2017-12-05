8f225a8e22ad29ae5df33caff11c628ea20fbde7
Duco Lindner <duckelekuuk@gmail.com>
2017-12-05 16:46:24 +0100

    Fixed https middleware

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,7 +15,7 @@ class HttpsProtocol
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && env('APP_ENV') === 'prod') {
+        if (!$request->secure() && env('APP_ENV') === 'production') {
             return redirect()->secure($request->getRequestUri());
         }
 
